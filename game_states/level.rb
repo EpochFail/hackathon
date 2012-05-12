bc5b71313f0d5b61ec049fa104254a8e1249bfa9
Jason Jones <jasonedwardjones@gmail.com>
2012-05-11 19:33:19 -0800

    editor

--- game_states/level.rb
+++ game_states/level.rb
@@ -1,6 +1,15 @@
 class Level < GameState  
   def setup
-    self.input = {:escape => :exit, :e => :edit } 
+    self.input = {:escape => :exit, :e => :edit }
+    
+    @file = File.join(ROOT, "level1.yml")
+    load_game_objects(:file => @file)
+    
+    @grid = [10,10]
+  end
+  
+  def edit
+    push_game_state GameStates::Edit.new(:grid => @grid, :file => @file)
   end
   
   def update
